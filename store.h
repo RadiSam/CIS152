854c8166a6a281842188d0db01be090a783b3e88
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-03-30 21:30:24 -0500

    Create store.h

---
+++ store.h
@@ -0,0 +1,22 @@
+#ifndef STORE_H
+#define STORE_H
+#include <player.h>
+#include <iostream>
+
+class Store {
+private:
+    //holds how many chances bought
+    int chances;
+    //holds how much paid
+    int bank;
+public:
+    Store();
+    //getters
+    int getChances();
+    int getBank();
+    //allows player to buy
+    void buyStuff(Player &name);
+
+};
+
+#endif // STORE_H
