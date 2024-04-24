71a736e51ff4843fb4ed1f22e22a0640bbfafb93
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-04-23 19:41:35 -0500

    Update1 roulette.h

--- roulette.h
+++ roulette.h
@@ -2,6 +2,8 @@
 #define ROULETTE_H
 #include <queue>
 #include <string>
+#include <player.h>
+#include <reportnodes.h>
 using namespace std;
 
 class Roulette {
@@ -13,10 +15,14 @@ public:
     Roulette();
     Roulette(int size);
     ~Roulette();
-    //sets queue
-    queue<int> setQueue(int size);
-    //returns results of gamble
-    string getResult(int chances);
+    //sets queue with random numbers
+    void setQueue(int size);
+    //returns string of gamble results and updates info
+    string getResult(int chances, Player& player, ReportNodes* node);
+    //checks roulette inbound condition
+    bool queueAvailable(int chances, queue<int> wheel);
+    //returns queue
+    queue<int> getWheel();
 };
 
 #endif // ROULETTE_H
