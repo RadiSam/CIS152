683260596b088041aefc74c48f943aa19145cd52
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-04-23 19:41:18 -0500

    Update1 roulette.cpp

--- roulette.cpp
+++ roulette.cpp
@@ -2,37 +2,60 @@
 #include<queue>
 #include<ctime>
 #include<cstdlib>
+#include<iostream>
 
 //constructors&destructor
 Roulette::Roulette() {
-    roulette = setQueue(20);
+    setQueue(20);
 }
-
 Roulette::Roulette(int size) {
-    roulette = setQueue(size);
+    setQueue(size);
 }
 Roulette::~Roulette() {}
 
-// returns queue of random ints when given size int
-queue<int> Roulette::setQueue(int size) {
+// sets queue with given size of random ints with max of size
+void Roulette::setQueue(int size) {
     srand(time(0));
     queue<int> prizes;
-
     for (int i = 0; i < size; i++) {
-        int prize = (rand() % 100) + 1;
+        int prize = (rand() % size) + 1;
         prizes.push(prize);
     }
-    return prizes;
+    roulette = prizes;
 }
 
-// returns string of results, roulette start funct
-string Roulette::getResult(int chances) {
+// returns string of results, also starts the roulette
+string Roulette::getResult(int chances, Player& player, ReportNodes* node) {
     string result = "Here is the result of your luck!\n";
-
+    int totalEarned = 0;
+    //nothing happens when input is 0
+    if (chances == 0) {return "Looks like nothing was entered.";}
+    //plays roulette
     for (int i = 0; i < chances; i++) {
         result.append(to_string(roulette.front()) + " ");
+        totalEarned += roulette.front();
         roulette.pop();
     }
-
+    //updates info
+    player.setPocket(player.getPocket() + totalEarned);
+    node->setMoneyEarned(node->getMoneyEarned() + totalEarned);
+    node->setRResult(result.substr(33, result.size()));
     return result;
+}
+
+// returns true when chances is less than or equal to wheel size
+bool Roulette::queueAvailable(int chances, queue<int> wheel) {
+    int wheelSize = wheel.size();
+    if (chances > wheelSize) {
+        cout << "Woah hold on, that's too many chances the wheel can handle." <<
+            " Try again with a smaller amount next time(Max is 20)." << endl;
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+//returns wheel
+queue<int> Roulette::getWheel() {
+    return roulette;
 }
