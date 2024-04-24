bffbca97216b9a1202792405d943ed6975cdf65c
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-04-23 19:40:35 -0500

    Update1 reportnodes.cpp

--- reportnodes.cpp
+++ reportnodes.cpp
@@ -1,31 +1,30 @@
 #include<reportnodes.h>
 
-//constructor&destructor
+//constructors&destructor
 ReportNodes::ReportNodes() {
     moneySpent = 0;
     chancesSpent = 0;
     moneyEarned = 0;
+    round = 0;
+    plinkoResult = "N/A";
+    rouletteResult = "N/A";
+    next = nullptr;
+}
+ReportNodes::ReportNodes(int round) {
+    moneySpent = 0;
+    chancesSpent = 0;
+    moneyEarned = 0;
+    this->round = round;
+    plinkoResult = "N/A";
+    rouletteResult = "N/A";
     next = nullptr;
 }
 ReportNodes::~ReportNodes() {}
 
-//getters
-int ReportNodes::getMoneySpent() const{
-    return moneySpent;
-}
-int ReportNodes::getChancesSpent() const{
-    return chancesSpent;
-}
-int ReportNodes::getMoneyEarned() const{
-    return moneyEarned;
-}
-ReportNodes* ReportNodes::getNext() const {
-    return next;
-}
-int ReportNodes::getRound() const {
-    return round;
-}
 //setters
+void ReportNodes::setRound(int currRound) {
+    round = currRound;
+}
 void ReportNodes::setMoneySpent(int money) {
     moneySpent = money;
 }
@@ -35,9 +34,36 @@ void ReportNodes::setChancesSpent(int chances) {
 void ReportNodes::setMoneyEarned(int money) {
     moneyEarned = money;
 }
+void ReportNodes::setPResult(string result) {
+    plinkoResult = result;
+}
+void ReportNodes::setRResult(string result) {
+    rouletteResult = result;
+}
 void ReportNodes::setNext(ReportNodes *nextNode) {
     next = nextNode;
 }
-void ReportNodes::setRound(int currRound) {
-    round = currRound;
+
+//getters
+int ReportNodes::getRound() const {
+    return round;
 }
+int ReportNodes::getMoneySpent() const{
+    return moneySpent;
+}
+int ReportNodes::getChancesSpent() const{
+    return chancesSpent;
+}
+int ReportNodes::getMoneyEarned() const{
+    return moneyEarned;
+}
+string ReportNodes::getPResult() const {
+    return plinkoResult;
+}
+string ReportNodes::getRResult() const {
+    return rouletteResult;
+}
+ReportNodes* ReportNodes::getNext() const {
+    return next;
+}
+
