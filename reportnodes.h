ae35a44070c4886af591838ae1523c46fb90b1d2
RadiSam <144635117+RadiSam@users.noreply.github.com>
2024-04-23 19:40:54 -0500

    Update1 reportnodes.h

--- reportnodes.h
+++ reportnodes.h
@@ -1,5 +1,7 @@
 #ifndef REPORTNODES_H
 #define REPORTNODES_H
+#include <string>
+using namespace std;
 
 // Node class for linkedlist of reports
 class ReportNodes {
@@ -9,23 +11,30 @@ private:
     int moneySpent;
     int chancesSpent;
     int moneyEarned;
+    string plinkoResult;
+    string rouletteResult;
     ReportNodes* next;
 public:
+    //constructors&destructor
     ReportNodes();
+    ReportNodes(int round);
     ~ReportNodes();
     //setters
+    void setRound(int currRound);
     void setMoneySpent(int money);
     void setChancesSpent(int chances);
     void setMoneyEarned(int money);
+    void setPResult(string result);
+    void setRResult(string result);
     void setNext(ReportNodes* nextNode);
-    void setRound(int currRound);
     //getters
+    int getRound() const;
     int getMoneySpent() const;
     int getChancesSpent() const;
     int getMoneyEarned() const;
+    string getPResult() const;
+    string getRResult() const;
     ReportNodes* getNext() const;
-    int getRound() const;
-
 };
 
 #endif // REPORTNODES_H
